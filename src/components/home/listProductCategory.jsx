f9d1280a8493ed53bbaf03e769a6a544ccc7cdb4
BalaguerRC <86633462+BalaguerRC@users.noreply.github.com>
2023-10-04 13:55:30 -0400

    homeview, imageList

--- src/components/home/listProductCategory.jsx
+++ src/components/home/listProductCategory.jsx
@@ -1,6 +1,12 @@
 import {
   Alert,
   Backdrop,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Chip,
   CircularProgress,
   Divider,
   Grid,
@@ -8,6 +14,7 @@ import {
   ImageList,
   ImageListItem,
   ImageListItemBar,
+  Link,
   ListSubheader,
   Snackbar,
   Typography,
@@ -17,6 +24,39 @@ import { Box } from "@mui/system";
 import { useEffect, useState } from "react";
 import InfoIcon from "@mui/icons-material/Info";
 import { useNavigate } from "react-router-dom";
+
+const productS = [
+  {
+    id: 1,
+    name: "test1",
+    price: 200,
+    img: desconocido,
+  },
+  {
+    id: 2,
+    name: "test2",
+    price: 200,
+    img: desconocido,
+  },
+  {
+    id: 3,
+    name: "test3",
+    price: 200,
+    img: desconocido,
+  },
+  {
+    id: 4,
+    name: "test4",
+    price: 200,
+    img: desconocido,
+  },
+  {
+    id: 5,
+    name: "test5",
+    price: 200,
+    img: desconocido,
+  },
+];
 
 const ListProductCategory = () => {
   const getToken = localStorage.getItem("Token");
@@ -80,92 +120,331 @@ const ListProductCategory = () => {
 
   return (
     <>
-      <Box sx={{ backgroundColor: "#22414B", flexGrow: 1 }}>
-        <Box
-          sx={{
-            justifyContent: "center",
-            alignItems: "center",
-            p: 7,
-            margin: "auto",
-            maxWidth: 1100,
-          }}
-        >
-          <Grid container spacing={5}>
-            <Grid item marginRight={1.8}>
-              <Box sx={{ maxWidth: 488, maxHeight: 328, paddingTop: 2 }}>
-                <Typography
-                  variant="h4"
-                  sx={{ fontSize: "3rem", fontWeight: 550 }}
-                  gutterBottom
-                >
-                  Productos
-                </Typography>
-                <Typography variant="subtitle1" gutterBottom>
-                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                  assumenda excepturi exercitationem quasi. In deleniti eaque
-                  aut repudiandae et a id nisi.
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid item xs sm container>
-              {/**content */}
-              <ImageList
-                sx={{
-                  width: "auto",
-                  maxWidth: 500,
-                  minWidth: 200,
-                  height: 400,
-                }}
-                cols={2}
-              >
-                <ImageListItem key="Subheader" cols={2}>
-                  <ListSubheader component="div">Products</ListSubheader>
-                </ImageListItem>
-                {image &&
-                  image.map((item) => (
-                    <ImageListItem key={item.id}>
+      <Box sx={{ backgroundColor: "#22414B" }}>
+        <Grid container direction={"column"} p={8} spacing={4}>
+          <Grid item>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Typography variant="h4" sx={{ fontWeight: 700 }}>
+                Latest Products
+              </Typography>
+              <Button variant="text">
+                <Link href="#" underline="hover">
+                  View More
+                </Link>
+              </Button>
+            </Box>
+          </Grid>
+          <Grid item>
+            <Grid
+              container
+              direction={"row"}
+              justifyContent={"center"}
+              spacing={5}
+              alignItems={"center"}
+            >
+              {productS.map((data) => (
+                <Grid item key={data.id}>
+                  <Card sx={{ maxWidth: 245, maxHeight: 400, borderRadius: 5 }}>
+                    <CardMedia>
                       <img
-                        src={`${
-                          item.image == null
-                            ? desconocido
-                            : item.image
-                        }?w=248&fit=crop&auto=format`}
-                        srcSet={`${
-                          item.image == null
-                            ? desconocido
-                            : item.image
-                        }?w=248&fit=crop&auto=format&dpr=2 2x`}
-                        alt={item.name}
-                        loading="lazy"
+                        src={data.img}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          borderRadius: 5,
+                        }}
                       />
-                      <ImageListItemBar
-                        title={item.name}
-                        subtitle={item.author}
-                        actionIcon={
-                          <IconButton
-                            sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                            aria-label={`info about ${item.name}`}
-                            onClick={() => {
-                              setLoading(!Loading);
-                              setTimeout(() => {
-                                setLoading(!Loading);
-                                navigate("/products/details/" + item.id);
-                              }, 1000);
-                            }}
-                          >
-                            <InfoIcon />
-                          </IconButton>
-                        }
-                      />
-                    </ImageListItem>
-                  ))}
-              </ImageList>
+                    </CardMedia>
+                    <CardContent sx={2}>
+                      <Box display={"flex"}>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {data.name}
+                        </Typography>
+                        <Chip
+                          label={"new"}
+                          color="secondary"
+                          sx={{ ml: 2 }}
+                          size="small"
+                        />
+                      </Box>
+                    </CardContent>
+                    <CardActions sx={{ justifyContent: "space-between", p: 2 }}>
+                      <Typography variant="body2" color="text.secondary">
+                        RD$ {data.price}
+                      </Typography>
+                      <Button variant="contained" sx={{ borderRadius: 3 }}>
+                        Buy
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
             </Grid>
           </Grid>
-        </Box>
+        </Grid>
       </Box>
 
-      <Box
+      <Box>
+        <Grid container direction={"column"} p={8} spacing={4}>
+          <Grid item>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Typography variant="h4" sx={{ fontWeight: 700 }}>
+                Video Games
+              </Typography>
+              <Button variant="text">
+                <Link href="#" underline="hover">
+                  View More
+                </Link>
+              </Button>
+            </Box>
+          </Grid>
+          <Grid item>
+            <Grid
+              container
+              direction={"row"}
+              justifyContent={"center"}
+              spacing={5}
+              alignItems={"center"}
+            >
+              {productS.map((data) => (
+                <Grid item key={data.id}>
+                  <Card sx={{ maxWidth: 245, maxHeight: 400, borderRadius: 5 }}>
+                    <CardMedia>
+                      <img
+                        src={data.img}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          borderRadius: 5,
+                        }}
+                      />
+                    </CardMedia>
+                    <CardContent sx={2}>
+                      <Box display={"flex"}>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {data.name}
+                        </Typography>
+                        <Chip
+                          label={"new"}
+                          color="secondary"
+                          sx={{ ml: 2 }}
+                          size="small"
+                        />
+                      </Box>
+                    </CardContent>
+                    <CardActions sx={{ justifyContent: "space-between", p: 2 }}>
+                      <Typography variant="body2" color="text.secondary">
+                        RD$ {data.price}
+                      </Typography>
+                      <Button variant="contained" sx={{ borderRadius: 3 }}>
+                        Buy
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Box sx={{ backgroundColor: "#22414B" }}>
+        <Grid
+          container
+          direction={"row"}
+          p={8}
+          justifyContent={"space-between"}
+        >
+          <Grid item>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Typography variant="h4" sx={{ fontWeight: 700 }}>
+                Electronics
+              </Typography>
+              <Button variant="text">
+                <Link href="#" underline="hover">
+                  View More
+                </Link>
+              </Button>
+            </Box>
+
+            <ImageList
+              sx={{ width: "auto", maxWidth: 500, minWidth: 200, height: 400 }}
+              cols={2}
+            >
+              {productS.map((item) => (
+                <ImageListItem key={item.id}>
+                  <img
+                    src={`${
+                      item.img == null ? "/src/assets/signo.png" : item.img
+                    }?w=248&fit=crop&auto=format`}
+                    srcSet={`${
+                      item.img == null ? "/src/assets/signo.png" : item.img
+                    }?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    alt={item.name}
+                    loading="lazy"
+                  />
+                  <ImageListItemBar
+                    title={item.name}
+                    actionIcon={
+                      <IconButton
+                        sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                        aria-label={`info about ${item.name}`}
+                      >
+                        <InfoIcon />
+                      </IconButton>
+                    }
+                  />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </Grid>
+          <Grid item>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Typography variant="h4" sx={{ fontWeight: 700 }}>
+                Clothes
+              </Typography>
+              <Button variant="text">
+                <Link href="#" underline="hover">
+                  View More
+                </Link>
+              </Button>
+            </Box>
+
+            <ImageList
+              sx={{ width: "auto", maxWidth: 500, minWidth: 200, height: 400 }}
+              cols={2}
+            >
+              {productS.map((item) => (
+                <ImageListItem key={item.id}>
+                  <img
+                    src={`${
+                      item.img == null ? "/src/assets/signo.png" : item.img
+                    }?w=248&fit=crop&auto=format`}
+                    srcSet={`${
+                      item.img == null ? "/src/assets/signo.png" : item.img
+                    }?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    alt={item.name}
+                    loading="lazy"
+                  />
+                  <ImageListItemBar
+                    title={item.name}
+                    actionIcon={
+                      <IconButton
+                        sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                        aria-label={`info about ${item.name}`}
+                      >
+                        <InfoIcon />
+                      </IconButton>
+                    }
+                  />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </Grid>
+          <Grid item>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Typography variant="h4" sx={{ fontWeight: 700 }}>
+                Shoes
+              </Typography>
+              <Button variant="text">
+                <Link href="#" underline="hover">
+                  View More
+                </Link>
+              </Button>
+            </Box>
+
+            <ImageList
+              sx={{ width: "auto", maxWidth: 500, minWidth: 200, height: 400 }}
+              cols={2}
+            >
+              {productS.map((item) => (
+                <ImageListItem key={item.id}>
+                  <img
+                    src={`${
+                      item.img == null ? "/src/assets/signo.png" : item.img
+                    }?w=248&fit=crop&auto=format`}
+                    srcSet={`${
+                      item.img == null ? "/src/assets/signo.png" : item.img
+                    }?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    alt={item.name}
+                    loading="lazy"
+                  />
+                  <ImageListItemBar
+                    title={item.name}
+                    actionIcon={
+                      <IconButton
+                        sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                        aria-label={`info about ${item.name}`}
+                      >
+                        <InfoIcon />
+                      </IconButton>
+                    }
+                  />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Snackbar
+        open={Error ? open : null}
+        autoHideDuration={5000}
+        onClose={() => setOpen(!open)}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      >
+        <Alert variant="filled" severity="warning">
+          Token Expirado
+        </Alert>
+      </Snackbar>
+      <Backdrop
+        sx={{ color: "#7eb8cf", zIndex: (theme) => theme.zIndex.drawer }}
+        open={Loading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
+  );
+};
+
+{
+  /**
+  <Box
         sx={{
           justifyContent: "center",
           alignItems: "center",
@@ -193,7 +472,6 @@ const ListProductCategory = () => {
             </Box>
           </Grid>
           <Grid item xs sm container>
-            {/**content */}
             {image1 == null ? null : (
               <Grid item xs container direction="column">
                 <Grid item xs>
@@ -214,14 +492,10 @@ const ListProductCategory = () => {
                         <ImageListItem key={item.id}>
                           <img
                             src={`${
-                              item.image == null
-                                ? desconocido
-                                : item.image
+                              item.image == null ? desconocido : item.image
                             }?w=248&fit=crop&auto=format`}
                             srcSet={`${
-                              item.image == null
-                                ? desconocido
-                                : item.image
+                              item.image == null ? desconocido : item.image
                             }?w=248&fit=crop&auto=format&dpr=2 2x`}
                             alt={item.name}
                             loading="lazy"
@@ -252,7 +526,6 @@ const ListProductCategory = () => {
               </Grid>
             )}
 
-            {/**#2 */}
             {image2 == null ? (
               <>Holaaaaaaa </>
             ) : (
@@ -274,14 +547,10 @@ const ListProductCategory = () => {
                         <ImageListItem key={item.id}>
                           <img
                             src={`${
-                              item.image == null
-                                ? desconocido
-                                : item.image
+                              item.image == null ? desconocido : item.image
                             }?w=248&fit=crop&auto=format`}
                             srcSet={`${
-                              item.image == null
-                                ? desconocido
-                                : item.image
+                              item.image == null ? desconocido : item.image
                             }?w=248&fit=crop&auto=format&dpr=2 2x`}
                             alt={item.name}
                             loading="lazy"
@@ -330,8 +599,6 @@ const ListProductCategory = () => {
       >
         <CircularProgress color="inherit" />
       </Backdrop>
-    </>
-  );
-};
-
+   */
+}
 export default ListProductCategory;
